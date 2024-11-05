46d57008a1acd646c1292350ee02bc192a4d933e
ajohnson46 <119007383+ajohnson46@users.noreply.github.com>
2024-11-05 13:57:07 -0600

    Loaded the dictionary with the date, max, min, and mean temps. At this point, we are fully scraping one month of data.

--- scrape_weather.py
+++ scrape_weather.py
@@ -7,6 +7,8 @@ class GCWeatherParser(HTMLParser):
         self.weatherData = {}
         self.in_row = False
         self.in_th = False
+        self.column_counter = 0
+        self.date = 0
 
     def handle_starttag(self, tag, attrs):
         # th[scope=row] has the day. After, the data will be weather info. Non-numeric can be ignored.
@@ -15,8 +17,11 @@ class GCWeatherParser(HTMLParser):
             # I'll need to know that the tag is TH later...
             self.in_row = True
             self.in_th = True # Not just in TH, but specifically the ones we like
+            self.column_counter = 0
 
         # td, while in row, would be Max, then Min, then Mean, and then others. Need to count.
+        if tag == "td":
+            self.column_counter += 1 
 
     def handle_endtag(self, tag):
         if tag == "tr":
@@ -26,21 +31,32 @@ class GCWeatherParser(HTMLParser):
 
     def handle_data(self, data):
         data = data.strip()
+         # if it's not numeric, just return... (this is probably not the best way to do it)
+        try:
+            data = float(data)
+        except:
+            if data and self.in_th:
+                self.in_row = False # If row does not start with a number, this is not a row of data
+            return
         # When we see data, we need to know if we are in the TH...
         # "When we..." = We are here now, and can check what 'data' is or what our variables have
         # "...we need to know..." = There should be a variable that was set up previously
         # "...if we are in..." = There's a condition that is set up. We're looking for a boolean concept.
         if self.in_row:
             if self.in_th: # This is specifically the beginning of the row.
-                # if it's not numeric, just return... (this is probably not the best way to do it)
-                try:
-                    data = int(data)
-                except:
-                    return
-                self.weatherData[data] = []    
+                self.date = int(data)
+                # Creating the dictionary keys
+                self.weatherData[self.date] = {"max": None, "min": None, "mean": None} 
+            elif self.column_counter == 1:
+                self.weatherData[self.date]["max"] = data
+            elif self.column_counter == 2:
+                self.weatherData[self.date]["min"] = data
+            elif self.column_counter == 3:
+                self.weatherData[self.date]["mean"] = data
             else: # other parts of the row....
                 # We need to knw if we're in a td? The first, second, or third tds?
-                print("other data from row: "+data)
+                # print(f"other data from row: {data}")
+                pass
 
 parser = GCWeatherParser()
 
