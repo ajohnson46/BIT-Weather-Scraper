694fe78e99bed7e430562ed74d7d4b68850301ef
ajohnson46 <119007383+ajohnson46@users.noreply.github.com>
2024-12-03 13:44:55 -0600

    Box plot is showing up correctly now, added line plot that shows the data of the last month.

--- plot_operations.py
+++ plot_operations.py
@@ -15,8 +15,8 @@ class PlotOperations:
         for data in weather_data:
             month = data[1][5:7]
             if month not in monthly_data.keys():
-                # count of days. sum of avgs, max for the month, and the min for month
-                monthly_data[month] = [1,data[5],data[3],data[4]]
+                # count of days. sum of avgs, max for the month, and the min for month, list of raw averages
+                monthly_data[month] = [1,data[5],data[3],data[4],[]]
                 continue
             monthly_data[month][0] += 1
             monthly_data[month][1] += data[5]
@@ -24,6 +24,7 @@ class PlotOperations:
                 monthly_data[month][2] = data[3]
             if monthly_data[month][3] > data[4]:
                 monthly_data[month][3] = data[4]
+            monthly_data[month][4].append(data[5]) 
 
             print (data)
             print (data[1][5:7])
@@ -32,17 +33,25 @@ class PlotOperations:
 
         monthly_data = dict(sorted(monthly_data.items()))
 
+        data = []
+
         # count of days. sum of avgs, max for the month, and the min for month
         for key,item in monthly_data.items():
-            spread.append(item[2])
-            spread.append(item[3])
-            center.append(item[1]/item[0])
-            flier_high.append(item[2])
-            flier_low.append(item[3])
+            data.append(item[4])
+            # spread.append(item[2])
+            # spread.append(item[3])
+            # center.append(item[1]/item[0])
+            # flier_high.append(item[2])
+            # flier_low.append(item[3])
         
-        plot_data = np.concatenate((spread, center, flier_high, flier_low), 0)
-        print(plt.boxplot(plot_data))
+        # plot_data = np.concatenate((spread, center, flier_high, flier_low), 0)
+        plt.boxplot(data)
         plt.show()
+        plt.plot(data[0])
+        plt.show()
+
+        # d = np.random.normal(5,10,15)
+        # print(d)
 
 if __name__ == '__main__':
     plotter = PlotOperations()
