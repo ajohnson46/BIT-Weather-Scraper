97ffa45221b462da18b89903418bfecedc775b4f
Abi <abigailf03@hotmail.ca>
2024-12-06 13:10:43 -0600

    updates to save and fetch data

--- db_operations.py
+++ db_operations.py
@@ -30,43 +30,29 @@ class DBOperations:
         except Exception as e:
             print("Error creating table:", e)
 
-#wasn't in assignment requirements, added in just in case - from topic challenge on DBs
-   # def insert(self, sample_data):
-    #    """Insert the given data into the DB"""
-     #   try:
-      #      #query to insert data in weather tbl
-      #      sql = """insert into weather (sample_date, location, min_temp, max_temp, avg_temp)
-      #       values (?,?,?,?,?)"""
-      #      c = self.conn.cursor()
-       #     for date, data in sample_data.items():
-       #         location = data.get('Location', 'Unknown')
-        #        c.execute(sql, (date, location, data['Min'], data['Max'], data['Mean']))
-        #    self.conn.commit()
-        #    print("Data Inserted successfully.")
-        #except Exception as e:
-        #    print("Error inserting sample.", e)
-
-    def fetch_data(self):
-        """fetches the data currently in the DB
-        returns a list of tuples containing the data """
+    def fetch_data(self, year=None):
+        """Fetches data from the DB, optionally filtered by year."""
         try:
             with DBCM(self.db_name) as c:
-                #query to fetch all the data in the weather tbl
-                sql = "SELECT * FROM weather"
-                c.execute(sql)
-                #put into a tuple that contains the data, this is what is returned
+                if year:
+                   sql = "SELECT * FROM weather WHERE sample_date LIKE ?"
+                   c.execute(sql, (f"{year}%",))
+                else:
+                    sql = "SELECT * FROM weather"
+                    c.execute(sql)
                 results = c.fetchall()
-                print("Data fetched successfully")
+                print("Fetched data:", results)  # Debug output
                 return results
         except Exception as e:
-            print("Error fetching data.", e)
-            return []
+             print("Error fetching data:", e)
+             return []
         
     def save_data(self, data):
         """ saves new data to the DB only if it doesn't already exist
          if the record does exist, it will be updated """
         try:
             with DBCM(self.db_name) as c:
+                print(f"Saving data: {data}")  # Debug output
                 sql = """INSERT INTO weather (sample_date, location, min_temp, max_temp, avg_temp)
                  VALUES (?, ?, ?, ?, ?)
                  ON CONFLICT(sample_date, location) DO UPDATE SET
@@ -74,11 +60,9 @@ class DBOperations:
                  max_temp = excluded.max_temp,
                  avg_temp = excluded.avg_temp"""
                 c.execute(sql, (data["sample_date"], data["location"], data["min_temp"], data["max_temp"], data["avg_temp"]))
-                # Expected result. No need for message.
-                # print("Data saved or updated successfully.")
-                # NOTE: Could use logging.debug to show these only if we set it to debug mode.
+                
         except Exception as e:
-            print(f"When saving {data["sample_date"]}: {e}")
+            print(f"When saving {data['sample_date']}: {e}")
 
     def purge_data(self):
         """ purge all the data from the DB for when program fetches all new weather data 
